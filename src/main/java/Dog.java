0c06b4c07b62aa4e9429a594f6d71c7f78c61e1c
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-2.local>
2019-10-30 23:26:16 -0400

    done for the night

---
+++ src/main/java/Dog.java
@@ -0,0 +1,7 @@
+import io.zipcoder.polymorphism.Pets;
+
+public class Dog extends Pets {
+
+    public String speak(){return "Bark!";}
+
+}
---
+++ src/main/java/io/zipcoder/polymorphism/Pets.java
@@ -0,0 +1,17 @@
+package io.zipcoder.polymorphism;
+
+public class Pets {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String speak(){
+        return "Huh?";
+    }
+}
